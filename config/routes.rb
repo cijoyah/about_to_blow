4e12ccf12f1f05333b7b31b337c085a9f2c5e22c
Chris Ijoyah <chris@ijoyah.com>
2015-07-03 13:52:57 +0100

    added playlist controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :playlists 
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
