f12a0a053231d992472f6777cb78bd714b4308a9
Chris Ijoyah <chris@ijoyah.com>
2015-07-15 01:39:09 +0100

    added image uploading to registration forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+  
   has_many :playlists
+
+  has_attached_file :avatar, :styles => { :large => "500x500#", :medium => "300x300#", :thumb => "100x100#" }
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
