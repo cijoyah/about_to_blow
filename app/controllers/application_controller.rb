78ee8b0119aef1d42e0c51962d6dada7ec427d64
Chris Ijoyah <chris@ijoyah.com>
2015-07-15 18:25:22 +0100

    sorted out problem with ne users not being able to register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_paramters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_paramters
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :location, :passowrd, :password_confirmation, :avatar)}
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :email, :location, :passowrd, :password_confirmation, :current_password, :avatar )}
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :location, :password, :password_confirmation, :avatar)}
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar)}
   end
-
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
-
 end
