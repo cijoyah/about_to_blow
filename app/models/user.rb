a6e4ed44edf6e9343cdcb3c1913614bd0f231bcb
Chris Ijoyah <chris@ijoyah.com>
2015-07-16 14:57:40 +0100

    attempting to resolve Paperclip issue with Amazon’s S3 european buckets

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,20 @@ class User < ActiveRecord::Base
   
   has_many :playlists
 
-  has_attached_file :avatar, :styles => { :large => "500x500#", :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { 
+    :large => "500x500#", 
+    :medium => "300x300#", 
+    :thumb => "100x100#" 
+    }, 
+    :default_url => "/images/:style/missing.png",
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
+    :s3_protocol => "https",
+    :path => ":class/:id/:basename_:style.:extension",
+    :bucket => "about-to-blow",
+    :url  => ":s3_eu_url"
+
+
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
