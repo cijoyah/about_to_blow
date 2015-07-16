ff4793efede1e27e4a0d63b44749484906162ca9
Chris Ijoyah <chris@ijoyah.com>
2015-07-16 15:38:17 +0100

    4th attempt to resolve Paperclip issue with Amazon’s S3 european buckets

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,10 +2,5 @@ Paperclip.interpolates(:s3_eu_url) do |att, style|
     "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.S3_BUCKET_NAME}/#{att.path(style)}"
 end
 
-module AWS
-    module S3
-        DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
-    end
-end
 
 
