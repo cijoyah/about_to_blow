843ed976427ce1d0af9364dfb1282c4c66b8f907
Chris Ijoyah <chris@ijoyah.com>
2015-07-16 15:33:58 +0100

    3nd attempt to resolve Paperclip issue with Amazon’s S3 european buckets

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,8 @@ class User < ActiveRecord::Base
     :medium => "300x300#", 
     :thumb => "100x100#" 
     }, 
-    :default_url => "/images/:style/missing.png",
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] },
-    :s3_protocol => "https",
-    :path => ":class/:id/:basename_:style.:extension",
-    :url  => ":s3_eu_url"
+    :default_url => "/images/:style/missing.png"
+  
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,14 @@ Rails.application.configure do
 
   #Paperclip Amazon S3 upload
   config.paperclip_defaults = {
-    :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :storage => :s3,
+    :s3_protocol => "https",
+    :path => ":class/:id/:basename_:style.:extension",
+    :url  => ":s3_eu_url"
   }
 end
