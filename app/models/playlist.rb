bf82e8c070b4d7127f3fc3f39f05a861e7e70e73
Chris Ijoyah <chris@ijoyah.com>
2015-08-06 12:44:20 +0100

    uncommented content type and file name in playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -11,8 +11,8 @@ class Playlist < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_attached_file :mp3
-  validates_attachment :mp3
-  #, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] }
-  #, :file_name => { :matches => [/mp3\Z/] }
+  validates_attachment :mp3,
+  :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] },
+  :file_name => { :matches => [/mp3\Z/] }
 
 end
