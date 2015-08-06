2e50a27ea2334fba5472030a4c25b71b54ac0c87
Chris Ijoyah <chris@ijoyah.com>
2015-08-06 13:31:03 +0100

    added s3 protocol https, to resolve insecure image issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    # :s3_protocol => "https",
+    :s3_protocol => "https",
     :path => ":class/:id/:basename_:style.:extension",
     :url  => ":s3_eu_url"
   }
